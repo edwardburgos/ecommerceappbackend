bda904ef751156fbb68bab416de7f39e46bb5e1e
EdwardBurgos <edwardpbn@gmail.com>
2021-12-10 15:20:30 -0500

    graphql server improved with new fields added and getCategoriesProduct and getProduct methods implemented in Query resolver

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import { sequelizeObject } from "./db";
 import { gql } from 'apollo-server';
-import { ProductType, ReturnedSlideType } from "./extras/types";
+import { ReturnedSlideType, QueryResult } from "./extras/types";
 import { ApolloServer } from "apollo-server";
 import sequelize, { Model } from 'sequelize'
 import dataLoad from "./extras/dataLoad";
+import { Op } from "sequelize";
 
 const { conn } = sequelizeObject;
-const { product, category, photo, slide, company, category_child, category_children_child } = sequelizeObject.models;
+const { product, category, photo, slide, company, category_child, category_children_child, category_product, product_photo } = sequelizeObject.models;
 
 (async function databaseConfiguration() {
   try {
@@ -25,12 +26,15 @@ const { product, category, photo, slide, company, category_child, category_child
         name: String!
         showInNav: Boolean!
         level: String!
+        url: String!
       }
       type SecondLevelCategory {
         id: ID!
+        categoryId: ID!
         name: String!
         showInNav: Boolean!
         level: String!
+        url: String!
         categories: [Category]
       }
       type FirstLevelCategory {
@@ -38,12 +42,26 @@ const { product, category, photo, slide, company, category_child, category_child
         name: String!
         showInNav: Boolean!
         level: String!
+        url: String!
         categories: [SecondLevelCategory]
       }
-      type Product {
-        name: String!
+      type ProductSimple {
         id: ID!
-        categoryId: String!
+        photo: String!
+        name: String!
+        price: Float!
+        currency: String!
+        url: String!
+      }
+      type ProductComplete {
+        id: ID!
+        photos: [String!]
+        name: String!
+        description: String
+        price: Float!
+        currency: String!
+        stars: Float
+        url: String!
       }
       type Photo {
         url: String!
@@ -67,10 +85,12 @@ const { product, category, photo, slide, company, category_child, category_child
         productCount: Int!
         categoriesCount: Int!
         photosCount: Int!
-        findProductsByName(name: String!): [Product]
+        findProductsByName(name: String!): [ProductSimple]
         allSlides: [Slide]
         getCompany: Company
         getMenu: [FirstLevelCategory]
+        getCategoriesProducts(urls: [String!]): [ProductSimple]
+        getProduct(url: String!): ProductComplete
       }
     `
     // GraphQL resolvers
@@ -79,7 +99,7 @@ const { product, category, photo, slide, company, category_child, category_child
         productCount: async () => await product.count(),
         categoriesCount: async () => await category.count(),
         photosCount: async () => await photo.count(),
-        findProductsByName: async (root: ProductType, args: { name: string }) => await product.findAll({
+        findProductsByName: async (parent: undefined, args: { name: string }) => await product.findAll({
           where: {
             name: sequelize.where(sequelize.fn('LOWER', sequelize.col('name')), 'LIKE', '%' + args.name.toLowerCase() + '%')
           }
@@ -104,19 +124,28 @@ const { product, category, photo, slide, company, category_child, category_child
         },
         getMenu: async () => {
           // First level categories
-          const firstLevelCategories = await category.findAll({ where: { level: '1' } })
+          const firstLevelCategories = await category.findAll({
+            where: { level: '1' }, order: [
+              ['order', 'ASC']
+            ]
+          })
           const firstLevelCategoriesIds = firstLevelCategories.map(e => e.getDataValue('id'))
 
           // First second level relationships
           const firstSecondLevelCategories = await Promise.all(firstLevelCategoriesIds.map(e => {
             return new Promise<{ id: string, categoryId: string, CategoryChildId: string }[] | null>(async (resolve, reject) => {
               try {
-                const firstSecondLevelCategories = await category_child.findAll({ where: { categoryId: e } })
+                const firstSecondLevelCategories = await category_child.findAll({
+                  where: { categoryId: e }, order: [
+                    ['order', 'ASC']
+                  ]
+                })
                 resolve(firstSecondLevelCategories.length ? firstSecondLevelCategories.map(e => {
                   const id = e.getDataValue('id');
                   const categoryId = e.getDataValue('categoryId');
                   const CategoryChildId = e.getDataValue('CategoryChildId');
-                  return { id, categoryId, CategoryChildId }
+                  const order = e.getDataValue('order')
+                  return { id, categoryId, CategoryChildId, order }
                 }) : null)
               } catch (e) {
                 reject(e)
@@ -150,8 +179,12 @@ const { product, category, photo, slide, company, category_child, category_child
           const firstSecondRelationshipsAndThirdLevelCategory = await Promise.all(firstSecondLevelCategoriesIds.map(e => {
             return new Promise<{ categoryId: string, categoryChildId: string }[] | null>(async (resolve, reject) => {
               try {
-                const relationshipsFound = await category_children_child.findAll({ where: { categoryChildId: e } })
-                resolve(relationshipsFound.length ? relationshipsFound.map(e => { return { categoryId: e.getDataValue('categoryId'), categoryChildId: e.getDataValue('categoryChildId') } }) : null)
+                const relationshipsFound = await category_children_child.findAll({
+                  where: { categoryChildId: e }, order: [
+                    ['order', 'ASC']
+                  ]
+                })
+                resolve(relationshipsFound.length ? relationshipsFound.map(e => { return { categoryId: e.getDataValue('categoryId'), categoryChildId: e.getDataValue('categoryChildId'), order: e.getDataValue('order') } }) : null)
               } catch (error) {
                 reject(error)
               }
@@ -183,6 +216,7 @@ const { product, category, photo, slide, company, category_child, category_child
               name: e.getDataValue('name'),
               showInNav: e.getDataValue('showInNav'),
               level: e.getDataValue('level'),
+              url: e.getDataValue('url'),
               categories: firstSecondLevelCategories.filter(el => el && (el[0].categoryId === e.getDataValue('id')))[0]?.map(e => {
                 const secondCategoryInfo = secondLevelCategoriesFound.filter(el => el && (el.getDataValue('id') === e.CategoryChildId))[0]
                 const firstSecondRelationShipId = e.id
@@ -195,10 +229,12 @@ const { product, category, photo, slide, company, category_child, category_child
                   }
                 })
                 return {
-                  id: secondCategoryInfo!.getDataValue('id'),
+                  id: firstSecondRelationShipId,
+                  categoryId: secondCategoryInfo!.getDataValue('id'),
                   name: secondCategoryInfo!.getDataValue('name'),
                   showInNav: secondCategoryInfo!.getDataValue('showInNav'),
                   level: secondCategoryInfo!.getDataValue('level'),
+                  url: secondCategoryInfo!.getDataValue('url'),
                   categories: thirdCategories.map(id => {
                     return thirdLevelCategoriesFound.filter(e => e && (e.getDataValue('id') === id))[0]
                   }).map(e => {
@@ -207,12 +243,90 @@ const { product, category, photo, slide, company, category_child, category_child
                       name: e!.getDataValue('name'),
                       showInNav: e!.getDataValue('showInNav'),
                       level: e!.getDataValue('level'),
+                      url: e!.getDataValue('url')
                     }
                   })
                 }
               })
             }
           })
+        },
+        getCategoriesProducts: async (parent: undefined, args: { urls: string[] }) => {
+          try {
+            const categoriesFound = await category.findAll({ where: { url: args.urls } })
+            const categoriesFoundIds = categoriesFound.map(e => e.getDataValue('id'))
+            const results = await conn.query(`SELECT * FROM "products" AS "product" WHERE
+            (SELECT COUNT(*) FROM category_products WHERE category_products."productId" = product.id) >= ${categoriesFoundIds.length} AND
+            (SELECT COUNT(*) FROM category_products WHERE category_products."productId" = product.id AND 
+            (${categoriesFoundIds.map((e, index) => index ? ` OR category_products."categoryId" = '${e}'` : `category_products."categoryId" = '${e}'`).join(' ')})) >= ${categoriesFoundIds.length}
+            LIMIT 20;`, {
+              model: product,
+            })
+            const photos = await Promise.all(results.map(e => {
+              return new Promise(async (resolve, reject) => {
+                try {
+                  const photoFound = await product_photo.findOne({
+                    where: {
+                      [Op.and]: [
+                        { productId: e.getDataValue('id') },
+                        { order: 0 }
+                      ]
+                    }
+                  })
+                  const photoURL = await photo.findOne({
+                    where: { id: photoFound?.getDataValue('photoId') }
+                  })
+                  resolve(photoURL?.getDataValue('url'))
+                } catch (e) {
+                  reject(e)
+                }
+              })
+            }))
+            return results.map((e, index) => {
+              return {
+                id: e.getDataValue('id'),
+                name: e.getDataValue('name'),
+                photo: photos[index],
+                url: e.getDataValue('url'),
+                price: e.getDataValue('price'),
+                currency: e.getDataValue('currency')
+              }
+            })
+          } catch (e) {
+            console.log(e)
+          }
+        },
+        getProduct: async (parent: undefined, args: { url: string }) => {
+          try {
+            const productFound = await product.findOne({ where: { url: args.url } })
+            const photosIds = await product_photo.findAll({
+              where: { productId: productFound?.getDataValue('id') }, order: [
+                ['order', 'ASC']
+              ]
+            })
+            const photosURLs = await Promise.all(photosIds.map(e => { 
+              return new Promise(async (resolve, reject) => {
+                try {
+                  const photoFound = await photo.findOne({ where: {id: e.getDataValue('photoId')}})
+                  resolve(photoFound!.getDataValue('url'))
+                } catch(e) {
+                  reject(e)
+                }
+              })
+            }))
+            return productFound ? {
+              id: productFound.getDataValue('id'),
+              name: productFound.getDataValue('name'),
+              url: productFound.getDataValue('url'),
+              photos: photosURLs,
+              description: productFound.getDataValue('description'),
+              price: productFound.getDataValue('price'),
+              currency: productFound.getDataValue('currency'),
+              stars: productFound.getDataValue('stars')
+            } : null
+          } catch (e) {
+            console.log(e)
+          }
         }
       }
     }
